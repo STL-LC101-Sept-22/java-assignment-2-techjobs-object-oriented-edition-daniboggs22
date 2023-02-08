ee5f41a894280533602b91b4f3180792bc9e1bcb
Daneille Boggs <daniboggs22@gmail.com>
2023-02-08 11:46:02 -0600

    Created test for empty constructor.

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,7 +1,11 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.Job;
+
+import static org.junit.Assert.assertNotEquals;
 
 /**
  * Created by LaunchCode
@@ -9,4 +13,15 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class JobTest {
 
+    @Test
+    public void testSettingJobId(){
+        String spec = "Test the empty constructor: the ids of two new job objects should not be equal.";
+        Job test_job = new Job();
+        Job job_two = new Job();
+        assertNotEquals(spec, test_job.getId(), job_two.getId());
+    }
+
+
+
 }
+
