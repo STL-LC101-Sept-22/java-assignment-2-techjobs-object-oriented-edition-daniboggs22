8ac31996e3b9e5d2434710053833d3e0cb14ad3a
Daneille Boggs <daniboggs22@gmail.com>
2023-02-27 19:59:53 -0600

    commit before updating tests to assertEquals.

--- src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
+++ src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
@@ -3,10 +3,10 @@ package org.launchcode.techjobs.oo;
 import java.util.Objects;
 
 public class CoreCompetency extends JobField{
-        public CoreCompetency() {
-            super();
-
-        }
+//        public CoreCompetency() {
+//            super();
+//
+//        }
         public CoreCompetency(String value) {
             super(value);
         }
--- src/main/java/org/launchcode/techjobs/oo/Location.java
+++ src/main/java/org/launchcode/techjobs/oo/Location.java
@@ -4,9 +4,9 @@ import java.util.Objects;
 
 public class Location extends JobField{
 
-    public Location() {
-      super();
-    }
+//    public Location() {
+//      super();
+//    }
 
     // TODO: Add a constructor that takes a string as a parameter and assigns it to the 'value' field. The
     //  constructor should also call the empty constructor in order to initialize the 'id' field.
