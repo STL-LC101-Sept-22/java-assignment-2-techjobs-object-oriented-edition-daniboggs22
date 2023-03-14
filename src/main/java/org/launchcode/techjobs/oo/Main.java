5d470ef6e9dc16449ee4ebf6b196f6e0554e2975
Daneille Boggs <daniboggs22@gmail.com>
2023-03-14 14:24:54 -0500

    Removed remaining specs and updated last test to pass.

--- src/main/java/org/launchcode/techjobs/oo/Main.java
+++ src/main/java/org/launchcode/techjobs/oo/Main.java
@@ -5,24 +5,17 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        //remove empty job1 when toString job method completed
-       //Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
        Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
        Job job2 = new Job("Web Developer", new Employer("LaunchCode"), new Location("St. Louis"), new PositionType("Front-end developer"), new CoreCompetency("JavaScript"));
-       Job job3 = new Job("Web Developer", new Employer("Apple"), new Location("Miami"), new PositionType("Front-end developer"), new CoreCompetency("JavaScript"));
-//        Job job3 = new Job("Ice cream tester", new Employer(""), new Location("Home"), new PositionType("UX"), new CoreCompetency("Tasting ability"));
-//
-//        ArrayList<Job> jobs = new ArrayList<>();
-//        jobs.add(job1);
-//        jobs.add(job2);
-//        jobs.add(job3);
-//
-//        for (Job job : jobs){
-//            System.out.println(job);
-//        }
-        System.out.println("~~~~~~~~~" + job1.toString());
-        System.out.println(job2.toString());
-        System.out.println(job3.toString());
-    }
+       Job job3 = new Job("Ice cream tester", new Employer(""), new Location("Home"), new PositionType("UX"), new CoreCompetency("Tasting ability"));
 
+        ArrayList<Job> jobs = new ArrayList<>();
+        jobs.add(job1);
+        jobs.add(job2);
+        jobs.add(job3);
+
+        for (Job job : jobs){
+            System.out.println(job);
+        }
+    }
 }
--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -21,7 +21,6 @@ public class JobTest {
     }
     @Test
     public void testJobConstructorSetsAllFields(){
-        String spec = "Test the full job constructor: the classes and values are set correctly";
         Job aJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"),
                 new PositionType("Quality control"), new CoreCompetency("Persistence"));
 //TODO Use assertTrue and assertEquals statements to test that the constructor correctly
@@ -39,16 +38,14 @@ public class JobTest {
     }
     @Test
     public void testJobsForEquality(){
-        String spec = "Two job objects are Not equal if all fields are identical except their id values.";
         Job aJob = new Job("Front End Developer", new Employer("LaunchCode"), new Location("St.Louis"),
                 new PositionType("Web Developer"), new CoreCompetency("Being Awesome"));
         Job jobTwo = new Job("Front End Developer", new Employer("LaunchCode"), new Location("St.Louis"),
                 new PositionType("Web Developer"), new CoreCompetency("Being Awesome"));
-        assertNotEquals(spec, aJob, jobTwo);
+        assertFalse(aJob.equals(jobTwo));
     }
     @Test
     public void testToStringStartsAndEndsWithNewLine(){
-        String spec = "toString method starts and ends with a new line";
         Job aJob = new Job("Front End Developer", new Employer("LaunchCode"), new Location("St.Louis"),
                 new PositionType("Web Developer"), new CoreCompetency("Being Awesome"));
       String jobFormat = aJob.toString();
@@ -70,15 +67,11 @@ public class JobTest {
    //TODO If a field is empty, the method should add, 'Data not available' after the label.
    @Test
     public void testToStringHandlesEmptyField() {
-        String spec = "If a field is empty 'Data not available' is included after the label";
         Job aJob = new Job("Front End Developer", new Employer("LaunchCode"), new Location("St.Louis"),
                 new PositionType("Web Developer"), new CoreCompetency(""));
         aJob.toString();
         String expected = aJob.getCoreCompetency().getValue().toString();
-       System.out.println(expected);
         assertEquals(expected,"Data not available");
-
     }
-
 }
 
